a091bc7c15ccaaa69114671666cb8ebad1057bde
mehvass <mehdivasseti@storks.com.tr>
2026-06-01 10:45:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,33 +8,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/pay", (req, res) => {
-  const orderId = "ORDER123";
-  const amount = "100";
-
-  const MID = "6700972665";
-  const TID = "67C36594";
-  const POSNET_ID = "1010082528833803";
-
-  res.send(`
-    <html>
-      <body>
-        <h2>Güvenli Ödeme</h2>
-        <form method="POST" action="https://setmpos.ykb.com/PosnetWebService/XML">
-          <input type="hidden" name="mid" value="${MID}" />
-          <input type="hidden" name="tid" value="${TID}" />
-          <input type="hidden" name="posnetId" value="${POSNET_ID}" />
-          <input type="hidden" name="amount" value="${amount}" />
-          <input type="hidden" name="orderId" value="${orderId}" />
-
-          <button type="submit">Yapı Kredi ile Öde</button>
-        </form>
-      </body>
-    </html>
-  `);
+  res.send("PAY OK");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server çalışıyor, port:", PORT);
 });
